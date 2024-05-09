02d652d5ae9f8e5d2b8809d98137d36d95319e8d
Leonardo de Souza <contato@leonardosouza.com.br>
2024-05-09 17:26:26 -0300

    Commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Seexyfy | O mundo liberal você encontra aqui</title>
+    <title>Seexfy</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/style.css" rel="stylesheet">
     <link rel="icon" type="image/png" href="assets/images/favicon.png">
