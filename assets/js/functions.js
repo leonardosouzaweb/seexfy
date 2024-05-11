b5b4e6a91cbf205dc223635b9dc4084c894f2a79
Leonardo de Souza <contato@leonardosouza.com.br>
2024-05-11 02:00:09 -0300

    commit

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -6,22 +6,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const agreeBtn = document.querySelector(".step1 button");
     const checkboxes = document.querySelectorAll(".form-check input[type='checkbox']");
 
-    // Esconde o step2 inicialmente
     step2.style.display = "none";
 
-    // Função para mostrar o step2 e esconder o step1
     function showStep2() {
         step1.style.display = "none";
         step2.style.display = "block";
     }
 
-    // Função para mostrar o step1 e esconder o step2
     function showStep1() {
         step2.style.display = "none";
         step1.style.display = "block";
     }
 
-    // Adiciona ou remove a classe 'active' na form-check quando o input é marcado ou desmarcado
     function handleCheckbox(event) {
         const checkbox = event.target;
         const formCheck = checkbox.parentElement;
@@ -32,18 +28,27 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    // Adiciona event listener para os botões voltar e continuar
+    function handleFormCheckClick(event) {
+        const formCheck = event.currentTarget;
+        const checkbox = formCheck.querySelector("input[type='checkbox']");
+        checkbox.checked = !checkbox.checked;
+        handleCheckbox({ target: checkbox });
+    }
+
     backBtn.addEventListener("click", showStep1);
     continueBtn.addEventListener("click", showStep2);
 
-    // Adiciona event listener para o botão "Eu Concordo"
     agreeBtn.addEventListener("click", showStep2);
 
-    // Adiciona event listener para cada checkbox
     checkboxes.forEach(function (checkbox) {
         checkbox.addEventListener("change", handleCheckbox);
     });
+
+    document.querySelectorAll('.form-check').forEach(function (formCheck) {
+        formCheck.addEventListener('click', handleFormCheckClick);
+    });
 });
+
 
 
 
