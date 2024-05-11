e2141cf6315ecb768ca4675be97de4c07a6c0f48
Leonardo de Souza <contato@leonardosouza.com.br>
2024-05-11 02:04:20 -0300

    commit

---
+++ includes/head.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="pt">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+    <title>Seexfy</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="assets/images/favicon.png">
+</head>
--- index.php
+++ index.php
@@ -1,13 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Seexfy</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/css/style.css" rel="stylesheet">
-    <link rel="icon" type="image/png" href="assets/images/favicon.png">
-</head>
+<?php 
+    include_once 'includes/head.php';
+?>
 <body>
     <div class="login">
         <div class="wrapper">
