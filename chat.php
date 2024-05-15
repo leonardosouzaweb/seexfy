4e22b28f3c54d2d7592a789721d2fa21bd8e6af3
Leonardo de Souza <contato@leonardosouza.com.br>
2024-05-15 02:54:56 -0300

    commit

---
+++ chat.php
@@ -0,0 +1,25 @@
+<?php 
+    include_once 'includes/head.php';
+?>
+<body>
+    <div class="empty">
+        <img src="assets/images/logo.svg">
+    </div>
+    <div class="home">
+        <div class="wrapper">
+            <?php 
+                include_once 'includes/topMenu.php';
+            ?>
+            <div class="content">
+                <h1>Chat</h1>
+               
+            </div>
+            <?php 
+                include_once 'includes/bottomMenu.php';
+            ?>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/functions.js"></script>
+</body>
+</html>
