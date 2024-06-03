37206467a3be9d51b7f99c5eacd8db48e1b861cb
Leonardo de Souza <contato@leonardosouza.com.br>
2024-06-03 09:50:14 -0300

    ajuste db

--- config/db.php
+++ config/db.php
@@ -1,13 +1,25 @@
 <?php
-$servername = "localhost";  // Nome do servidor MySQL
-$username = "root";         // Nome de usuário do MySQL
-$password = "root";             // Senha do MySQL
-$dbname = "seexfy";         // Nome do banco de dados
+// Detecta o hostname
+$hostname = gethostname();
 
-// Crie a conexão
+if ($hostname === 'localhost' || $hostname === 'seu-hostname-de-desenvolvimento') {
+    // Configuração para ambiente de desenvolvimento
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "seexfy";
+} else {
+    // Configuração para ambiente de produção
+    $servername = "p3plzcpnl505377.prod.phx3.secureserver.net";
+    $username = "seexfy";
+    $password = "0A7B2LvP7mFx8Q4dDbo2Q9o";
+    $dbname = "seexfy";
+}
+
+// Cria a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verifique a conexão
+// Verifica a conexão
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
