0c0224d975cb04ff5a15394f36029da9d84d3eed
Leonardo de Souza <contato@leonardosouza.com.br>
2024-06-11 21:44:14 -0300

    commit

--- includes/head.php
+++ includes/head.php
@@ -13,7 +13,7 @@
     <meta name="HandheldFriendly" content="true">
     <title>Seexfy</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?php echo $base_url; ?>assets/css/style.css?v=<?php echo $version; ?>" rel="stylesheet">
+    <link href="<?php echo $base_url; ?>assets/css/style.css" rel="stylesheet">
     <link rel="icon" type="image/png" href="<?php echo $base_url; ?>assets/images/favicon.png">
 
     <link rel="manifest" href="<?php echo $base_url; ?>manifest.json">
