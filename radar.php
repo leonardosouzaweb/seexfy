e73a404e80c1c2686b9553bf577c8f330924f8d6
Leonardo de Souza <contato@leonardosouza.com.br>
2024-06-14 01:45:39 -0300

    commit

--- radar.php
+++ radar.php
@@ -32,7 +32,7 @@ $conn->close();
                     <img src="<?php echo $base_url; ?>assets/images/icons/IconRadar2x.svg">
                     <h2>Radar</h2>
                     <p>Clique no botão abaixo para encontrar pessoas próximas a você.</p>
-                    <button id="activeLocation">Ativar localização</button>
+                    <button id="activeLocation" class="btn-active-location">Ativar localização</button>
                 </div>
             </div>
         </div>
@@ -42,9 +42,19 @@ $conn->close();
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="assets/js/functions.js"></script>
-    <script>
+        <script>
         $(document).ready(function() {
             $('button#activeLocation').click(function() {
+                var button = $(this);
+
+                // Verifica se o botão já está em processo de carregamento para evitar cliques repetidos
+                if (button.hasClass('loading') || button.is(':disabled')) {
+                    return; // Se estiver em processo de carregamento ou desabilitado, sai da função
+                }
+
+                // Adiciona classe de carregamento e desabilita o botão
+                button.addClass('loading').attr('disabled', 'disabled');
+
                 if (navigator.geolocation) {
                     navigator.geolocation.getCurrentPosition(function(position) {
                         var latitude = position.coords.latitude;
@@ -66,12 +76,18 @@ $conn->close();
                                 error: function(xhr, status, error) {
                                     alert('Erro ao salvar localização.');
                                     console.error(xhr.responseText);
+                                },
+                                complete: function() {
+                                    // Remove classe de carregamento e habilita o botão novamente
+                                    button.removeClass('loading').removeAttr('disabled');
                                 }
                             });
                         });
                     });
                 } else {
                     alert('Geolocalização não é suportada por este navegador.');
+                    // Remove classe de carregamento e habilita o botão novamente
+                    button.removeClass('loading').removeAttr('disabled');
                 }
             });
 
