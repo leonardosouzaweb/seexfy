b92b3eab024afc15a426adbe954269bec0e8bcfd
Leonardo de Souza <contato@leonardosouza.com.br>
2024-06-14 03:21:30 -0300

    commit

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -52,18 +52,61 @@ menuIcon.addEventListener("click", function() {
 
 function setActiveClass() {
     var path = window.location.pathname;
+    console.log('Current path:', path); // Log para depurar o caminho atual
     var fileName = path.split('/').pop().split('.')[0];
+    console.log('File name:', fileName); // Log para depurar o nome do arquivo
     var menuItems = document.querySelectorAll('.bottomMenu div');
 
     menuItems.forEach(function(item) {
-        if (item.id === fileName) {
-            item.classList.add('active');
-        } else {
-            item.classList.remove('active');
+        var icon = item.querySelector('.menu-icon');
+        if (icon) {
+            if (item.id === fileName) {
+                item.classList.add('active');
+                console.log('Adding active class to:', item.id); // Log para depurar a adição da classe active
+                // Trocar a imagem quando estiver ativo
+                switch (fileName) {
+                    case 'home':
+                        icon.src = 'assets/images/icons/iconHomeActive.svg';
+                        break;
+                    case 'eventos':
+                        icon.src = 'assets/images/icons/iconEventActive.svg';
+                        break;
+                    case 'radar':
+                        icon.src = 'assets/images/icons/iconRadarActive.svg';
+                        break;
+                    case 'chat':
+                        icon.src = 'assets/images/icons/iconChatActive.svg';
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                item.classList.remove('active');
+                // Voltar para a imagem original quando não estiver ativo
+                switch (item.id) {
+                    case 'home':
+                        icon.src = 'assets/images/icons/iconHome.svg';
+                        break;
+                    case 'eventos':
+                        icon.src = 'assets/images/icons/iconEvent.svg';
+                        break;
+                    case 'radar':
+                        icon.src = 'assets/images/icons/iconRadar.svg';
+                        break;
+                    case 'chat':
+                        icon.src = 'assets/images/icons/iconChat.svg';
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     });
 }
 
 document.addEventListener('DOMContentLoaded', setActiveClass);
 window.addEventListener('popstate', setActiveClass);
+
+
+
 
