02a83b34bf4b30a262727be195663d18af0e537a
Leonardo de Souza <contato@leonardosouza.com.br>
2024-11-15 00:29:10 -0300

    commit

--- components/monetization.php
+++ components/monetization.php
@@ -27,17 +27,16 @@
                 <h5>R$ 0,00</h5>
             </div>
         </div>
-
-        <p class="mt-3">Com a Seexfy, você ganha recompensas por cada indicação feita através 
-        do seu link. Basta copiar o link e compartilhá-lo. Sempre que um usuário 
-        realizar a assinatura utilizando o seu link, você receberá 20% do valor 
-        do pagamento. E o melhor: você continuará ganhando os 20% enquanto o 
-        usuário indicado permanecer ativo.</p>
-
+        <a href="create.php"><button>Criar plano de assinatura</button></a>
+        <hr>
         <div class="invite">
             <h5>Link de Convite</h5>
             <input type="text" class="form-control" value="https://seexfy.com/invite/389189">
-            <span> <img src="<?php echo $base_url; ?>/images/icons/black/iconCopy.svg"> Clique aqui para copiar o link</span>
+            <p class="mt-3">Com a Seexfy, você ganha recompensas por cada indicação feita através 
+            do seu link. Basta copiar o link e compartilhá-lo. Sempre que um usuário 
+            realizar a assinatura utilizando o seu link, você receberá 20% do valor 
+            do pagamento. E o melhor: você continuará ganhando os 20% enquanto o 
+            usuário indicado permanecer ativo.</p>
         </div>
     </div>
 </div>
