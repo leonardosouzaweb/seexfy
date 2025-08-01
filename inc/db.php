6ce3483876c9446e02a23c85ede04fd01af0e14d
Leonardo de Souza <contato@leonardosouza.com.br>
2025-08-01 16:37:22 -0300

    ajuste banco

--- inc/db.php
+++ inc/db.php
@@ -1,8 +1,19 @@
 <?php
-$host = 'localhost';
-$db = 'seexfy';
-$user = 'root';
-$pass = 'root';
+// Detecta automaticamente o ambiente baseado na URL
+if (strpos($_SERVER['HTTP_HOST'], 'localhost') !== false) {
+    // Ambiente Local
+    $host = 'localhost';
+    $db   = 'seexfy';
+    $user = 'root';
+    $pass = 'root';
+} else {
+    // Ambiente Produção
+    $host = 'localhost';
+    $db   = 'leona497_sex';
+    $user = 'leona497_sex';
+    $pass = 'Leo2248228@';
+}
+
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
