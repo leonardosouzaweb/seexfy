0b59f0a7146f828698cefbb183a064d4f647f9e0
Leonardo de Souza <contato@leonardosouza.com.br>
2025-08-07 02:49:13 -0300

    Update globalHead.php

--- inc/globalHead.php
+++ inc/globalHead.php
@@ -15,7 +15,8 @@ if ($host == 'localhost') {
 <html lang="pt-BR">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
+
 
     <title>Seexfy | Rede de encontros e descobertas</title>
 
