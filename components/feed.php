419e9120052ca7611ccdcd87ad030509f6677123
Leonardo de Souza <contato@leonardosouza.com.br>
2025-08-07 02:57:04 -0300

    commit

--- components/feed.php
+++ components/feed.php
@@ -145,8 +145,7 @@ $emoji_map = [
 
   /* Classe para aplicar blur suave nas imagens dos posts */
   .blurred-post-images .contentImage img {
-    filter: blur(6px);
-    transition: filter 0.5s ease;
+    filter: blur(12px);
   }
 </style>
 
